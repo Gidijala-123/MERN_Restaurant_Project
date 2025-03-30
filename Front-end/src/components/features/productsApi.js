9a186f3207c5f76aa0d01c19ce5d5a21f38bef61
Bhargava Gidijala <85632729+Gidijala-123@users.noreply.github.com>
2025-03-30 17:17:43 +0530

    renamed images

--- Front-end/src/components/features/productsApi.js
+++ Front-end/src/components/features/productsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_API_URL || "http://localhost:1234"}/api/`,
+    baseUrl: `${process.env.REACT_APP_API_URL || "http://localhost:1234"}`,
   }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
